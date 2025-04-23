6b906c8dfda43867985759e0972637f909d8a93b
LKO <konopski@gmail.com>
2025-04-23 22:26:47 +0200

    enhance

--- 07/increment_version.sh
+++ 07/increment_version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -name "deploy.yaml" | xargs perl -pi -e 's/centos6-operations:0.0.(.*)/"centos6-operations:0.0." . ($1+1)/e '
+find . -name "deploy.yaml" | xargs perl -pi -e 's/centos6-(.*)operations:0.0.(.*)/"centos6-$1operations:0.0." . ($2+1)/e '
 
