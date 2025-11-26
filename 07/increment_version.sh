23e180be9b01f2507cbf71e9fe24f0ae37342af0
LKO <konopski@gmail.com>
2025-11-26 16:09:21 +0100

    enhanced semver incrementation

--- 07/increment_version.sh
+++ 07/increment_version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-find . -name "deploy.yaml" | xargs perl -pi -e 's/centos6-(.*)operations:0.0.(.*)/"centos6-$1operations:0.0." . ($2+1)/e '
+find . -name "deploy.yaml" | xargs perl -pi -e 's/(centos6-.*operations:)(\d+(?:\.\d+)*)\.(\d+)/$1 . $2 . "." . ($3+1)/e '
+find . -name "cron.yaml" | xargs perl -pi -e 's/(centos6-.*operations-cron:)(\d+(?:\.\d+)*)\.(\d+)/$1 . $2 . "." . ($3+1)/e '
 
